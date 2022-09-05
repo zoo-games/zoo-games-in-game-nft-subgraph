ee963aa5c0d289a35e7329441f818ce2e2e9778c
lolieatapple <lolieatapple@gmail.com>
2022-09-06 00:19:10 +0800

    finish

--- src/mapping.ts
+++ src/mapping.ts
@@ -26,70 +26,19 @@ import {
   Withdraw,
   WithdrawFee
 } from "../generated/GameCore/GameCore"
-import { ExampleEntity } from "../generated/schema"
+// import { ExampleEntity } from "../generated/schema"
+import { GameStatus, GameRegisterRequest } from "../generated/schema"
 
 export function handleAuditGame(event: AuditGame): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let game = GameRegisterRequest.load(event.params._gamePrimary.toHex());
+  if (game) {
+    if (event.params._enable) {
+      game.status = "AUDITED";
+    } else {
+      game.status = "REJECTED";
+    }
+    game.save();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._gamePrimary = event.params._gamePrimary
-  entity._enable = event.params._enable
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.DEFAULT_PLATFORM_FEE(...)
-  // - contract.OPERATOR_ROLE(...)
-  // - contract.banned(...)
-  // - contract.gameInfo(...)
-  // - contract.gameNonce(...)
-  // - contract.gameRoundInfo(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getRoundId(...)
-  // - contract.hasRole(...)
-  // - contract.isGame(...)
-  // - contract.isGameStartable(...)
-  // - contract.lockTime(...)
-  // - contract.mirrorBalance(...)
-  // - contract.mirrorDelegate(...)
-  // - contract.mirrorProxyAdmin(...)
-  // - contract.paused(...)
-  // - contract.playerInfo(...)
-  // - contract.supportsInterface(...)
-  // - contract.timeLock(...)
-  // - contract.totalFee(...)
-  // - contract.userInfo(...)
-  // - contract.vZOO(...)
 }
 
 export function handleConfigLockTime(event: ConfigLockTime): void {}
@@ -108,11 +57,30 @@ export function handleEmergWithdraw(event: EmergWithdraw): void {}
 
 export function handleEnterTimeLock(event: EnterTimeLock): void {}
 
-export function handleGameCancel(event: GameCancel): void {}
+export function handleGameCancel(event: GameCancel): void {
+  let status = GameStatus.load(event.params._roundId.toHex());
+  if (status) {
+    status.status = "CANCEL";
+    status.save();
+  }
+}
 
-export function handleGameEnd(event: GameEnd): void {}
+export function handleGameEnd(event: GameEnd): void {
+  let status = GameStatus.load(event.params._roundId.toHex());
+  if (status) {
+    status.status = "END";
+    status.save();
+  }
+}
 
-export function handleGameStart(event: GameStart): void {}
+export function handleGameStart(event: GameStart): void {
+  let status = new GameStatus(event.params._input._roundId.toHex());
+  status.status = "STARTED";
+  status.roundId = event.params._input._roundId;
+  status.gamePrimary = event.params._input._gamePrimary;
+  status.timestamp = event.block.timestamp;
+  status.save();
+}
 
 export function handleInitialized(event: Initialized): void {}
 
@@ -122,7 +90,21 @@ export function handlePause(event: Pause): void {}
 
 export function handlePaymentInGame(event: PaymentInGame): void {}
 
-export function handleRequestRegisterGame(event: RequestRegisterGame): void {}
+export function handleRequestRegisterGame(event: RequestRegisterGame): void {
+  let game = new GameRegisterRequest(event.transaction.from.toHex());
+  game.gamePrimary = event.transaction.from;
+  game.gameName = event.params._input.gameName;
+  game.gameDescription = event.params._input.gameDescription;
+  game.gameImageURL = event.params._input.gameImageURL;
+  game.gameCategory = event.params._input.gameCategory;
+  game.maxVZOO = event.params._input.maxVZOO;
+  game.minTimeCostPerRound = event.params._input.minTimeCostPerRound;
+  game.maxPlayerCountPerRound = event.params._input.maxPlayerCountPerRound;
+  game.email = event.params._input.email;
+  game.informationJsonURL = event.params._input.informationJsonURL;
+  game.status = "PENDING";
+  game.save();
+}
 
 export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
 
